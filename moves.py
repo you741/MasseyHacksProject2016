2fe2e6d79496491ec467487c0f7db3e7ced9ad81
Albert Zhan <albertzhan@gmail.com>
2016-02-27 14:03:47 -0500

    adding Move class
    have different types of moves?

---
+++ moves.py
@@ -0,0 +1,8 @@
+##handling
+class Move:    
+    def __init__ (self, damage, energy, time, cooldown):
+        self.damage = damage
+        self.energy = energy
+        self.time = time
+        self.cooldown = cooldown
+
