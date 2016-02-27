2f6003646f57bceda7e6e8df9ffcf8b3d55e5bf3
You Zhou <youzhouphoenix@hotmail.com>
2016-02-27 10:31:26 -0500

    initialize
    Has the scrolling feature

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from pygame import *
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = '70,25'
+screen = display.set_mode((1200,730))
+#====COLOR====#
+BLACK  = (0,0,0)
+#====IMAGES====#
+back1 = image.load("images/background1.png")
+#====MC VAR====#
+x,y = 600,500
+screen.blit(back1,(0,0))
+screen.set_clip(Rect(0,0,1200,730))
+running = True
+while running:
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+    kp = key.get_pressed()
+    if kp[K_RIGHT] and x < 3000:
+        x += 10
+    if kp[K_LEFT] and x > 600:
+        x -= 10
+    screen.blit(back1,(x*-1+600,0))
+    display.flip()
+quit()
