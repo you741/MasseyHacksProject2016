e9132037a799c5070051015d997d43187101c333
Jasper Hong <theawesomejasper1926@gmail.com>
2016-02-27 10:53:51 -0500

    Health bars added

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ x,y = 600,500
 screen.blit(back1,(0,0))
 screen.set_clip(Rect(0,0,1200,730))
 running = True
+hpbar1 = Rect(50,50,450,50)
+hpbar2 = Rect(700,50,450,50)
 while running:
     for e in event.get():
         if e.type == QUIT:
@@ -22,5 +24,7 @@ while running:
     if kp[K_LEFT] and x > 600:
         x -= 10
     screen.blit(back1,(x*-1+600,0))
+    draw.rect(screen,(0,255,0),hpbar1)
+    draw.rect(screen,(0,255,0),hpbar2)
     display.flip()
 quit()
